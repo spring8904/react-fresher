3edddd0657439bb2bcf97412f9cf26961abaf131
spring8904 <lamthang890123music@gmail.com>
2023-12-27 16:50:30 +0700

    Add @uidotdev/usehooks package and fix file naming convention

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import ErrorPage from './routes/error-page.jsx'
+import ErrorPage from './routes/ErrorPage.jsx'
 import Home from './routes/Home.jsx'
-import Users from './routes/Users.jsx'
 import Login from './routes/Login.jsx'
+import Users from './routes/Users.jsx'
 
 const App = () => {
   const router = createBrowserRouter([
